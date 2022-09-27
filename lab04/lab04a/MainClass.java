631d9282bde12a931ea5bbcccd22db239319d15d
Winter Ohlson <WinterOhlson04@gmail.com>
2022-09-27 13:44:45 -0500

    Created MainClass.java with the method main()

---
+++ lab04/lab04a/MainClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jehuv
+ */
+public class MainClass {
+    public static void main(String[] args)
+    {
+        
+    }
+}
